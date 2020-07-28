ac717beabe6025b166d0c51c13256b1128fd4fac
Shakoor Hussain Attari <binmushtaq@gmail.com>
2020-07-28 14:50:01 +0400

    chrome extion for amazon seller

---
+++ Selenium.Tests/AmazonTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium.Tests
+{
+    [TestClass]
+    public class AmazonTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddExtensions(@"D:\github\selenium-test-sample\drivers\cmjihoeplpkmlmbbiiognkceoechmand.crx");
+            driver = new ChromeDriver(@"D:\github\selenium-test-sample\drivers", options);
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.amazon.ae/";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
---
+++ Selenium.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("050ba3a2-5fab-424d-93e8-d5dcb76d8433")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
